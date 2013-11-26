23c99d44d29a7142ddbca1765b9af48e3f9e9143
spadaal <alessio.spadaro@ec.europa.eu>
2013-11-26 16:39:46 +0100

    Added ascii-based histogram Improved output readability

--- klust/src/test/java/org/as/beehive/klust/KlustTest2.java
+++ klust/src/test/java/org/as/beehive/klust/KlustTest2.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,15 +13,16 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.math3.ml.clustering.CentroidCluster;
 import org.apache.commons.math3.ml.clustering.Clusterable;
-import org.apache.commons.math3.ml.distance.DistanceMeasure;
 
 /**
  * Unit test for simple App.
  */
 public class KlustTest2 extends TestCase {
+
+    NumberFormat nf;
+
     /**
      * Create the test case
      * 
@@ -29,6 +31,9 @@ public class KlustTest2 extends TestCase {
      */
     public KlustTest2(String testName) {
 	super(testName);
+	nf = NumberFormat.getInstance();
+	nf.setMaximumFractionDigits(2);
+	nf.setMinimumFractionDigits(2);
     }
 
     /**
@@ -36,13 +41,6 @@ public class KlustTest2 extends TestCase {
      */
     public static Test suite() {
 	return new TestSuite(KlustTest2.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-	assertTrue(true);
     }
 
     protected List<String> loadData() {
@@ -93,81 +91,61 @@ public class KlustTest2 extends TestCase {
 
 	List<CentroidCluster<DPoint>> clust = kmpp.cluster(Arrays
 		.asList(points));
-	dumpCluster(clust);
+	dumpCluster(clust, false, true);
 
     }
 
-    public void _testKM() {
-	MyDistanceMeasure measure = new MyDistanceMeasure();
-	MyDistanceMeasure2 measure2 = new MyDistanceMeasure2();
-	int nclusters = 10;
-	Point[] points = getDataset();
-
-	System.out.println("Generating " + nclusters + " clusters ");
-	KMeansPlusPlusClusterer<Point> kmpp = new KMeansPlusPlusClusterer<Point>(
-		nclusters, 1000, measure);
-	kmpp.setCentroidComputer(new MyCentroidComputer());
-
-	KMeansPlusPlusClusterer<Point> kmpp2 = new KMeansPlusPlusClusterer<Point>(
-		nclusters, 1000, measure2);
-	kmpp.setCentroidComputer(new MyCentroidComputer());
-
-	List<CentroidCluster<Point>> clust = kmpp
-		.cluster(Arrays.asList(points));
-	dumpCluster(measure, clust);
-
-	System.out
-		.println("------------------------------------------------------");
-
-	List<CentroidCluster<Point>> clust2 = kmpp2.cluster(Arrays
-		.asList(points));
-	dumpCluster(measure2, clust2);
-    }
-
-    private void dumpCluster(DistanceMeasure measure,
-	    List<CentroidCluster<Point>> clust) {
-	for (CentroidCluster<Point> cc : clust) {
-	    if (cc.getCenter() instanceof Point) {
-		System.out.printf("Centr.\t %s\n",
-			formatAsBinary((Point) cc.getCenter()));
-	    } else {
-		System.out.println("Centr. " + cc.getCenter().getPoint());
-	    }
-	    for (Point p : cc.getPoints()) {
-		System.out.println("\t "
-			+ formatAsBinary(p)
-			+ " "
-			+ measure.compute(cc.getCenter().getPoint(),
-				p.getPoint()));
-	    }
-	    System.out.println();
-	    System.out.println();
-	}
-    }
-
-    private void dumpCluster(List<CentroidCluster<DPoint>> clust) {
+    private void dumpCluster(List<CentroidCluster<DPoint>> clust,
+	    boolean printDetails, boolean printHistogram) {
 	for (CentroidCluster<DPoint> cc : clust) {
 
-	    System.out.println("Centr. " + format(cc.getCenter()));
+	    System.out.println("C: " + format(cc.getCenter()) + " #elems:"
+		    + cc.getPoints().size());
+	    if (printHistogram) {
+		System.out.println();
 
-	    for (DPoint p : cc.getPoints()) {
-		System.out.println("\t " + format(p));
+		freqHist(cc.getCenter(), 0.10);
+	    }
+	    if (printDetails) {
+		System.out.println();
+
+		for (DPoint p : cc.getPoints()) {
+		    System.out.println("   " + format(p));
+		}
 	    }
 	    System.out.println();
 	    System.out.println();
 	}
+    }
+
+    private void freqHist(Clusterable center, double scale) {
+	for (long i = Math.round(Math.ceil(1 / scale)) - 1; i >= 0; i--) {
+	    StringBuilder sb = new StringBuilder("   ");
+	    for (double d : center.getPoint()) {
+		if (d >= scale * i) {
+		    sb.append(" *  |");
+		} else {
+		    sb.append("    |");
+		}
+	    }
+	    System.out.println(sb.toString());
+	}
+
     }
 
     private String format(Clusterable c) {
 	StringBuilder sb = new StringBuilder();
 	for (double d : c.getPoint()) {
-	    sb.append(((d > 0.5d) ? '1' : '0'));
+	    if (d == 0d) {
+		sb.append(" 0  |");
+	    } else if (d == 1.0d) {
+		sb.append(" 1  |");
+	    } else {
+		sb.append(nf.format(d));
+		sb.append('|');
+	    }
 	}
 	return sb.toString();
     }
 
-    private String formatAsBinary(Point p) {
-
-	return StringUtils.leftPad(Long.toBinaryString(p.getValue()), 24, '0');
-    }
 }
